1f90cb24af02c9b86511171780684dde46ddd4be
Jeffrey Burt <jeffrey.burt@karmacasting.com>
2018-05-15 14:30:38 -0400

    add HUSKY_ONLY_RUN_ON_BRANCH variable

--- templates/hook.sh
+++ templates/hook.sh
@@ -3,4 +3,9 @@
 # v{version} {platform}
 
 export HUSKY_GIT_PARAMS="$*"
+
+if [ "$HUSKY_ONLY_RUN_ON_BRANCH" ] && [ "$(git rev-parse --abbrev-ref HEAD)" != "$HUSKY_ONLY_RUN_ON_BRANCH" ]; then
+  exit 0
+fi
+
 {node} {script} `basename "$0"`
